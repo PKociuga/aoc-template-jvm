7a5979fc20ae4cfe544c3b736bb614889b4ecbe4
PKociuga <33624202+PKociuga@users.noreply.github.com>
2024-12-04 09:07:21 +0100

    Day1

--- day1/src/main/kotlin/Day1.kt
+++ day1/src/main/kotlin/Day1.kt
@@ -1,19 +1,28 @@
+import kotlin.math.abs
+
+const val SPACE = "   "
+
 fun main() {
     println(part1())
     println(part2())
 }
 
 fun part1(): Any {
-    return input
-
+    val (first, second) = input.map { line ->
+        line.substringBefore(SPACE).toInt() to line.substringAfter(SPACE).toInt()
+    }.unzip()
+    return first.sorted().zip(second.sorted()).sumOf { (item1, item2) -> abs(item1 - item2) }
 }
 
 fun part2(): Any {
-    return input.reversed()
+    val (first, second) = input.map { line ->
+        line.substringBefore(SPACE).toInt() to line.substringAfter(SPACE).toInt()
+    }.unzip()
+    return first.sumOf { number -> second.count { it == number } * number }
 }
 
-private val input: String by lazy {readInput()}
+private val input: List<String> by lazy {readInput()}
 
-private fun readInput(): String {
-    return object {}.javaClass.getResource("Day1.input").readText()
+private fun readInput(): List<String> {
+    return object {}.javaClass.getResourceAsStream("Day1.input")?.bufferedReader()?.readLines() ?: emptyList()
 }
