79f2d151651257eb2924ae91024c0257af9958b1
PKociuga <33624202+PKociuga@users.noreply.github.com>
2024-12-06 16:07:46 +0100

    Day2

--- day2/src/main/kotlin/Day2.kt
+++ day2/src/main/kotlin/Day2.kt
@@ -1,19 +1,49 @@
+import kotlin.math.abs
+
 fun main() {
     println(part1())
     println(part2())
 }
 
 fun part1(): Any {
-    return input
-
+    val result = input.map { line ->
+        line.split(" ").map { item -> item.toInt() }
+    }
+    return result.count { isSafe(it) }
 }
 
 fun part2(): Any {
-    return input.reversed()
+    val result = input.map { line ->
+        line.split(" ").map { item -> item.toInt() }
+    }
+    return result.count { isSafeDampened(it) }
 }
 
-private val input: String by lazy {readInput()}
+private fun isSafe(list: List<Int>): Boolean {
+    var isDescending = true
+    var isIncreasing = true
 
-private fun readInput(): String {
-    return object {}.javaClass.getResource("Day2.input").readText()
+    for(i in 0 until list.size - 1) {
+        val diff = abs(list[i] - list[i + 1])
+        if(diff < 1 || diff > 3) {
+            return false
+        }
+        if(list[i] < list[i + 1]) {
+            isDescending = false
+        } else if(list[i] > list[i + 1]) {
+            isIncreasing = false
+        }
+    }
+    return isDescending || isIncreasing
+}
+
+private fun isSafeDampened(report: List<Int>): Boolean =
+    report.indices.any { item ->
+        isSafe(report.filterIndexed { index, _ -> item != index })
+    }
+
+private val input: List<String> by lazy {readInput()}
+
+private fun readInput(): List<String> {
+    return object {}.javaClass.getResourceAsStream("Day2.input")?.bufferedReader()?.readLines() ?: emptyList()
 }
